a217d06c2a295e65d43eb90fb0888debc7bbdbaa
Julia26047 <126690732+Julia26047@users.noreply.github.com>
2024-10-24 15:40:44 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,41 +29,6 @@ struct Token {
     }
 };
 
-class Bor {
-public:
-    void add(const std::string& s) {
-        node *cur = root;
-        for (auto u : s) {
-            if (cur->next[u - 'a'] == nullptr) {
-                cur->next[u - 'a'] = new node();
-            }
-            cur = cur->next[u - 'a'];
-        }
-        ++cur->strings;
-    }
-    bool check(const std::string& s) {
-        node *cur = root;
-        for (auto u: s) {
-            if (cur->next[u - 'a'] != nullptr) {
-                cur = cur->next[u - 'a'];
-            } else return false;
-        }
-        if (cur->strings > 0) return 1;
-        else return 0;
-    }
-private:
-    struct node {
-        node *next[26];
-        int strings;
-        node() {
-            for (int i = 0; i < 26; i++) {
-                next[i] = nullptr;
-            }
-            strings = 0;
-        }
-    };
-    node *root = new node();
-};
 void create_bor(Bor bor) {
     std::ifstream fin("keyword.txt");
     std::string s;
