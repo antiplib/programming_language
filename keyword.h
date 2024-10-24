72f6bab1a91f1c6140e79d9f86d4e36e3be1d2f0
Julia26047 <126690732+Julia26047@users.noreply.github.com>
2024-10-24 15:39:42 +0300

    Add files via upload

---
+++ keyword.h
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unistd.h>
+
+#include <fcntl.h>
+
+#include <map>
+
+
+class Bor {
+public:
+    void add(const std::string& s) {
+        node *cur = root;
+        for (auto u : s) {
+            if (cur->next[u - 'a'] == nullptr) {
+                cur->next[u - 'a'] = new node();
+            }
+            cur = cur->next[u - 'a'];
+        }
+        ++cur->strings;
+    }
+    bool check(const std::string& s) {
+        node *cur = root;
+        for (auto u: s) {
+            if (cur->next[u - 'a'] != nullptr) {
+                cur = cur->next[u - 'a'];
+            } else return false;
+        }
+        if (cur->strings > 0) return 1;
+        else return 0;
+    }
+private:
+    struct node {
+        node *next[26];
+        int strings;
+        node() {
+            for (int i = 0; i < 26; i++) {
+                next[i] = nullptr;
+            }
+            strings = 0;
+        }
+    };
+    node *root = new node();
+};
