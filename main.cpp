d789e8c459f9078eee333147e49e6d8cf2ea7571
antiplib <vladotv12345@gmail.com>
2024-10-25 08:40:05 +0300

    main

--- main.cpp
+++ main.cpp
@@ -4,11 +4,15 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <unistd.h>
+
 #include <fcntl.h>
+
+#include <map>
 #include "keywords.h"
 
 enum TokenType {
-    KEYWORD, IDENTIFIER, LITERAL, OPERATOR, PUNCTUATION, POINT,COMMA, BRACKETS, OTHER
+    KEYWORD, IDENTIFIER, LITERAL, OPERATOR, PUNCTUATION, POINT, COMMA, BRACKETS, OTHER
 };
 
 struct Token {
@@ -24,6 +28,7 @@ struct Token {
         return os;
     }
 };
+
 void create_bor(Bor bor) {
     std::ifstream fin("keyword.txt");
     std::string s;
@@ -34,16 +39,15 @@ void create_bor(Bor bor) {
 
 
 }
-int  read_file() {
+
+int main() {
+    system("chcp 65001");
     std::ifstream in("code.txt");
     in.seekg (0, std::ios::end);
-    int size_file = in.tellg();
+    int size = 0;
+    size = in.tellg();
     in.close();
-    return size_file;
-}
-int main() {
-
-    int size = read_file();
+    // setlocale(LC_ALL, "ru_RU.UTF-8");
     int fd = open("code.txt", O_RDWR);
     char * buffer = (char*) malloc(size);
     read(fd, buffer, size);
@@ -81,10 +85,57 @@ int main() {
             if (keywords.check(lexeme)) {
                 tokens.push_back(Token(KEYWORD, lexeme, line));
             } else {
-                if (lexeme != " " &&  lexeme != "") {
-                    tokens.push_back(Token(OTHER, lexeme, line));
-                }
+                if (lexeme == " " &&  lexeme == "") {
 
+                   while (position < size && *(buffer + position) == ' ') {
+                       ++position;
+                   }
+                    lexeme = "";
+                    if(position >= size) {
+                        break;
+                    }
+                    lexeme += *(buffer + position);
+                    ++position;
+                    if(lexeme == "-" || lexeme == "+") {
+                        if(position < size) {
+                            if(*(buffer + position) == *(buffer + position - 1)) {
+                                lexeme += *(buffer + position);
+                                tokens.push_back(Token(OPERATOR, lexeme, line));
+                            }
+                            else {
+                                tokens.push_back(Token(OPERATOR, lexeme, line));
+                                --position;
+                            }
+                        }
+                    } else if(lexeme == "(" ||lexeme ==")") {
+                        tokens.push_back(Token(BRACKETS, lexeme, line));
+                    } else if(lexeme == ">" || lexeme == "<" || lexeme == "=" ||
+                        lexeme == "!" ) {
+                        if(position < size) {
+                            if(*(buffer + position) == '=') {
+                                lexeme += *(buffer + position);
+                                tokens.push_back(Token(OPERATOR, lexeme, line));
+                            } else {
+                                tokens.push_back(Token(OPERATOR, lexeme, line));
+                                --position;
+                            }
+                        }
+                    } else if(lexeme == ";" || lexeme == "{" || lexeme == "}") {
+                        tokens.push_back(Token(PUNCTUATION, lexeme, line));
+                        --position;
+                    } else if(lexeme == ".") {
+                        --position;
+                        tokens.push_back(Token(POINT, lexeme, line));
+                    } else if(lexeme == ",") {
+                        tokens.push_back(Token(COMMA, lexeme, line));
+                        --position;
+                    } else {
+                        while(position < size && (isalpha(*(buffer + position)) || *(buffer + position) -'0' >= 0 && *(buffer + position) - '0' <= 9)) {
+                            lexeme += *(buffer + position);
+                        }
+                        tokens.push_back(Token(LITERAL, lexeme, line));
+                    }
+                }
             }
 //            std::string s2 = "";
 //            s2 += *(buffer + position);
@@ -105,7 +156,6 @@ int main() {
         std::cout << token << std::endl;
     }
 
-
-
     return 0;
 }
+
