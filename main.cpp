d4be8e2864b0a10a7aaf2ec3b028dca38bbf111f
shosh <shosheva.yulya@mail.ru>
2024-11-12 15:24:46 +0300

    main

--- main.cpp
+++ main.cpp
@@ -19,4 +19,3 @@ int main(int argc, char* argv[]) {
     solve();
     return 0;
 }
-
--- solve.cpp
+++ solve.cpp
@@ -82,114 +82,128 @@ void solve(){
                 lexeme += *(buffer + position);
                 position++;
             }
+            if(lexeme[lexeme.size() - 1] == '\r') {
+                lexeme.erase(lexeme.size() - 1);
+            }
             if (keywords.check(lexeme)) {
                 tokens.push_back(Token(KEYWORD, lexeme, line));
             } else {
-                    if(lexeme.size() != 0) {
-                        tokens.push_back(Token(IDENTIFIER, lexeme, line));
-                        continue;
-                    }
-                   while (position < size && *(buffer + position) == ' ') {
-                       ++position;
-                   }
-                    // lexeme = "";
-                    if(position >= size) {
-                        break;
-                    }
-                    lexeme += *(buffer + position);
+                if(lexeme[lexeme.size() - 1] == '\r') {
+                    lexeme.erase(lexeme.size() - 1);
+                }
+                if(lexeme.size() != 0) {
+                    tokens.push_back(Token(IDENTIFIER, lexeme, line));
+                    continue;
+                }
+                while (position < size && *(buffer + position) == ' ') {
                     ++position;
-                    if(lexeme == "-" || lexeme == "+"|| lexeme == "*" || lexeme == "/") {
-                        if(position < size) {
-                            if(*(buffer + position) == *(buffer + position - 1)) {
-                                lexeme += *(buffer + position);
-                                tokens.push_back(Token(OPERATOR, lexeme, line));
-                            }
-                            else {
-                                tokens.push_back(Token(OPERATOR, lexeme, line));
-                                --position;
-                            }
-                        }
-                    }else if(lexeme[0] == '"') {
-                        tokens.push_back(Token(PUNCTUATION, lexeme, line));
-                        lexeme = "";
-                        while(position < size && *(buffer + position) != '"') {
+                }
+                lexeme = "";
+                if(position >= size) {
+                    break;
+                }
+                lexeme += *(buffer + position);
+                ++position;
+                if(lexeme == "-" || lexeme == "+"|| lexeme == "*" || lexeme == "/") {
+                    if(position < size) {
+                        if(*(buffer + position) == *(buffer + position - 1)) {
                             lexeme += *(buffer + position);
-                            ++position;
+                            tokens.push_back(Token(OPERATOR, lexeme, line));
                         }
-                        tokens.push_back(Token(LITERAL, lexeme, line));
-                        if(position < size && *(buffer + position) == '"') {
-                            lexeme = '"';
+                        else {
+                            tokens.push_back(Token(OPERATOR, lexeme, line));
+                            --position;
                         }
-                        tokens.push_back(Token(PUNCTUATION, lexeme, line));
+                    }
+                }else if(lexeme[0] == '"') {
+                    tokens.push_back(Token(PUNCTUATION, lexeme, line));
+                    lexeme = "";
+                    while(position < size && *(buffer + position) != '"') {
+                        lexeme += *(buffer + position);
+                        ++position;
+                    }
+                    tokens.push_back(Token(LITERAL, lexeme, line));
+                    if(position < size && *(buffer + position) == '"') {
+                        lexeme = '"';
+                    }
+                    tokens.push_back(Token(PUNCTUATION, lexeme, line));
 
-                    }else if(lexeme == "#") {
-                        tokens.push_back(Token(PUNCTUATION, lexeme, line));
-                        lexeme = "";
-                        while(position < size && *(buffer + position) != '\n') {
+                }else if(lexeme == "#") {
+                    tokens.push_back(Token(PUNCTUATION, lexeme, line));
+                    lexeme = "";
+                    while(position < size && *(buffer + position) != '\n') {
+                        lexeme += *(buffer + position);
+                        ++position;
+                    }
+                    if(lexeme[lexeme.size() - 1] == '\r') {
+                        lexeme.erase(lexeme.size() - 1);
+                    }
+                    tokens.push_back(Token(COMMENT, lexeme, line));
+                }
+                else if(lexeme == "(" || lexeme ==")") {
+                    tokens.push_back(Token(BRACKETS, lexeme, line));
+                    --position;
+                } else if(lexeme == ">" || lexeme == "<" || lexeme == "=" ||
+                          lexeme == "!"|| lexeme == "%" ) {
+                    if(position < size) {
+                        if(*(buffer + position) == '=') {
                             lexeme += *(buffer + position);
+                            tokens.push_back(Token(OPERATOR, lexeme, line));
+                        } else {
+                            tokens.push_back(Token(OPERATOR, lexeme, line));
+                            --position;
+                        }
+                    }
+                } else if(lexeme == ";" || lexeme == "{" || lexeme == "}") {
+                    tokens.push_back(Token(PUNCTUATION, lexeme, line));
+                    --position;
+                } else if(lexeme == ".") {
+                    --position;
+                    if(lexeme[lexeme.size() - 1] == '\r') {
+                        lexeme.erase(lexeme.size() - 1);
+                    }
+                    tokens.push_back(Token(POINT, lexeme, line));
+                } else if(lexeme == ",") {
+                    tokens.push_back(Token(COMMA, lexeme, line));
+                    if(lexeme[lexeme.size() - 1] == '\r') {
+                        lexeme.erase(lexeme.size() - 1);
+                    }
+                    --position;
+                } else if(*(buffer + position) != '\n'){
+                    while(position < size && *(buffer + position) != '\n'  && isalpha(*(buffer + position))) {
+                        lexeme += *(buffer + position);
+                        ++position;
+                    }
+                    if(lexeme.size() != 0 && ( lexeme[0] -'0' >= 0 && lexeme[0]- '0' <= 9)) {
+                        bool p = 0;
+                        while( position < size && ((*(buffer + position) -'0' >= 0 && *(buffer + position)- '0' <= 9) ||( *(buffer + position) == '.') && !p)) {
+                            lexeme += *(buffer + position);
+                            if(*(buffer + position) == '.') {
+                                p = 1;
+                            }
                             ++position;
                         }
+                        --position;
                         if(lexeme[lexeme.size() - 1] == '\r') {
                             lexeme.erase(lexeme.size() - 1);
                         }
-                        tokens.push_back(Token(COMMENT, lexeme, line));
                     }
-                else if(lexeme == "(" || lexeme ==")") {
-                        tokens.push_back(Token(BRACKETS, lexeme, line));
-                        --position;
-                    } else if(lexeme == ">" || lexeme == "<" || lexeme == "=" ||
-                        lexeme == "!"|| lexeme == "%" ) {
-                        if(position < size) {
-                            if(*(buffer + position) == '=') {
-                                lexeme += *(buffer + position);
-                                tokens.push_back(Token(OPERATOR, lexeme, line));
-                            } else {
-                                tokens.push_back(Token(OPERATOR, lexeme, line));
-                                --position;
-                            }
-                        }
-                    } else if(lexeme == ";" || lexeme == "{" || lexeme == "}") {
-                        tokens.push_back(Token(PUNCTUATION, lexeme, line));
-                        --position;
-                    } else if(lexeme == ".") {
-                        --position;
-                        tokens.push_back(Token(POINT, lexeme, line));
-                    } else if(lexeme == ",") {
-                        tokens.push_back(Token(COMMA, lexeme, line));
-                        --position;
-                    } else if(*(buffer + position) != '\n'){
-                        while(position < size && *(buffer + position) != '\n'  && isalpha(*(buffer + position))) {
-                            lexeme += *(buffer + position);
-                            ++position;
-                        }
-                        if(lexeme.size() != 0 && ( lexeme[0] -'0' >= 0 && lexeme[0]- '0' <= 9)) {
-                            bool p = 0;
-                            while( position < size && ((*(buffer + position) -'0' >= 0 && *(buffer + position)- '0' <= 9) ||( *(buffer + position) == '.') && !p)) {
-                                lexeme += *(buffer + position);
-                                if(*(buffer + position) == '.') {
-                                    p = 1;
-                                }
-                                ++position;
-                            }
-                        }
-                        // if(position < size && *(buffer + position) == '')
+                    // if(position < size && *(buffer + position) == '')
+                    if(lexeme[lexeme.size() - 1] == '\r') {
+                        lexeme.erase(lexeme.size() - 1);
+                    }
+                    if(lexeme.size() == 0) {
+                        continue;
+                    }
+                    if (lexeme != "" && lexeme != "\n") {
                         tokens.push_back(Token(LITERAL, lexeme, line));
-                    } else {
-                        line++;
                     }
+                } else {
+                    line++;
+                }
 
             }
-//            std::string s2 = "";
-//            s2 += *(buffer + position);
-//            if (*(buffer + position) != ' ' && *(buffer + position) != '\n') {
-//                tokens.push_back(Token(OTHER, s2, line));
-//            }
-//            if (*(buffer + position) == ';' || *(buffer + position) == '{' || *(buffer + position) == '}') {
-//                //std::cout << "push" << " " << s2 << " " << line << std::endl;
-//                tokens.push_back(Token(PUNCTUATION, s2, line));
-//            }
-
-
+            lexeme = "";
             position++;
         }
     }
