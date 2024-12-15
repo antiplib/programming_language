08df24d856c92e378f63ff57e87ed70e8921a41b
shosh <shosheva.yulya@mail.ru>
2024-12-15 22:04:14 +0300

    fixed error3

--- TID.h
+++ TID.h
@@ -22,17 +22,20 @@ struct parametr {
     std::string id;
     std::string value;
 };
+
+
 class Tid {
 public:
-    Tid(Tid* parent = nullptr) : parent_(parent) {}
-    void push_id(parametr& pr) {
+    void push_id(parametr& x) {
         for (parametr& i : parametrs) {
-            if (i.id == pr.id && i.type == pr.type) {
+            if (i.id == x.id && i.type == x.type) {
                 throw std::string ("the id already exists");
+
             }
         }
-        parametrs.push_back(pr);
+        parametrs.push_back(x);
     }
+
     std::pair <std::string, bool>  check_id(const std::string& id) {
         bool flag = 0;
         for (const parametr& i : parametrs) {
@@ -47,73 +50,52 @@ public:
         }
 
     }
-    Tid* parent_;
 private:
-    std::vector <parametr> parametrs;
-
+    std::vector<parametr> parametrs;
 };
-
 
 class Tree_Tid {
 public:
-    Tree_Tid() : current_scope_(new Tid()) {}
-
     ~Tree_Tid() {
-        for (auto scope : scopes_) {
-            delete scope;
+        for (auto* scope_ : scopes_) {
+            delete scope_;
         }
-        delete current_scope_;
     }
-
     void create_scope() {
-        Tid* new_scope = new Tid(current_scope_);
-        scopes_.push_back(new_scope);
-        current_scope_ = new_scope;
+        Tree_Tid* new_scope = new Tree_Tid();
+        new_scope->parent_ = cur_scope_;
+        cur_scope_->scopes_.push_back(new_scope);
+        cur_scope_ = new_scope;
     }
-
 
     void exit_scope() {
-        if (!scopes_.empty()) {
-            current_scope_ = scopes_.back()->parent_;
-            scopes_.pop_back();
-        } else {
-            throw std::string ("No more scopes to exit.");
+        if (cur_scope_ && cur_scope_->parent_) {
+            cur_scope_ = cur_scope_->parent_;
         }
     }
-    std::pair <std::string, bool> check_id(const std::string& id) {
-        Tid* scope = current_scope_;
-        bool flag = 0;
-        while (scope) {
-            std::string type;
-            bool fl;
-            if (scope->check_id(id).second == 0) {
-                //std::cout << "ERROR2" << std::endl;
-                return std::make_pair("", 0);
-            } else {
-                type = scope->check_id(id).first;
-            }
 
-            if (!type.empty()) {
-                flag = 1;
-                return std::make_pair(type, 1);
+    void push_id(parametr& x) {
+        cur_scope_->scope.push_id(x);
+    }
+    Tree_Tid* cur_scope_ = this;
+
+    std::pair <std::string, bool> check_id(const std::string& id) {
+        Tree_Tid* scope = cur_scope_;
+        std::string ans;
+        while (scope) {
+            ans = scope->scope.check_id(const_cast<std::string&>(id)).first;
+            if (!ans.empty()) {
+                return std::make_pair(ans, 1);
             }
             scope = scope->parent_;
         }
-        if (flag == 0) {
-            return std::make_pair("", 0);
-        }
-        //throw ("id is not found");
+        return std::make_pair("", 0);;
     }
-    void push_id(parametr& pr) {
-        current_scope_->push_id(pr);
-    }
-
-
-    Tid* current_scope_;
 
 private:
-    // Tid* current_scope_;
-    std::vector<Tid*> scopes_;
+    Tid scope;
+    Tree_Tid* parent_ = nullptr;
+    std::vector<Tree_Tid*> scopes_;
 };
 
 
